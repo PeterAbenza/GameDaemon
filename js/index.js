55850209a9d4cf81d4eec3bd462a104f6c129428
PeterAbenza <abenzpeter@gmail.com>
2024-08-13 23:48:40 -0300

    Sistema pontuação e reset.

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const player = document.querySelector('.player'); 
   const nuvens = document.querySelector('.nuvens'); 
   const pipe = document.querySelector('.pipe');
+  const pipeVoador = document.querySelector('.pipeVoador');
   const arvore = document.querySelector('.arvore');
   const gameOver = document.querySelector('.gameOver');
   const pressEnter = document.querySelector('.enter');
-  const audio = new Audio('musicaFundo.mp3');
+  const reset = document.querySelector('.reset');
+  const pontos = document.querySelector('.pontos');
+  const audio = new Audio('./sons/musicaFundo.mp3');
 
-  if (!player || !nuvens || !pipe || !arvore || !gameOver || !pressEnter) {
+  if (!player || !nuvens || !pipe || !pipeVoador || !arvore || !gameOver || !pressEnter || !reset || !pontos) {
     console.error('Um ou mais elementos não foram encontrados no DOM.');
     return;
   }
@@ -18,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
       nuvensPosition: nuvens.offsetLeft,
       arvorePosition: arvore.offsetLeft,
       pipePosition: pipe.offsetLeft,
+      pipeVoadorPosition: pipeVoador.offsetLeft,
       playerPosition: +window.getComputedStyle(player).bottom.replace("px", "")
     };
   };
@@ -27,9 +31,43 @@ document.addEventListener('DOMContentLoaded', () => {
         nuvensRect: nuvens.getBoundingClientRect(),
         arvoreRect: arvore.getBoundingClientRect(),
         pipeRect: pipe.getBoundingClientRect(),
+        pipeVoadorRect: pipeVoador.getBoundingClientRect(),
         playerRect: player.getBoundingClientRect()
     };
 };
+
+// pontuação
+const pontuacao = (initialPo) => {
+  let p = 0; 
+  let intervaloId;
+  pontos.style.fontSize = "20px";
+
+  const atualizarPontuacao = () => {
+    if (initialPo === true) {
+      p += 10; 
+      pontos.innerHTML = p; 
+    } else {
+      clearInterval(intervaloId); 
+      initialPo = null;
+    }
+  };
+
+  intervaloId = setInterval(atualizarPontuacao, 800);
+
+  return {
+    setPo: (novoPo) => {
+      initialPo = novoPo;
+      if (initialPo === false) {
+        clearInterval(intervaloId); 
+        pontos.innerHTML = p; 
+        pontos.style.fontsize = "x-larger" ;
+        initialPo = null; 
+      }
+    }
+  };
+};
+
+const controlePontuacao = pontuacao(false);
 
   // Função de animação do jogador
   const jump = () => {
@@ -37,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
       player.classList.add('jump');
       setTimeout(() => {
         player.classList.remove('jump');
-      }, 700); // Ajuste o tempo para corresponder ao tempo da animação
+      }, 700);
     } else {
       console.error('Elemento player não encontrado');
     }
@@ -45,22 +83,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Função de verificação de colisão
   const checkCollision = () => {
-    const { nuvensPosition, arvorePosition, pipePosition, playerPosition } = updatePositions();
-    const { nuvensRect, arvoreRect, pipeRect, playerRect } = updatePositions2();
+    const { nuvensPosition, arvorePosition, pipePosition, pipeVoadorPosition, playerPosition } = updatePositions();
+    const { nuvensRect, arvoreRect, pipeRect, pipeVoadorRect, playerRect } = updatePositions2();
 
     // Verifica se há sobreposição entre o pipe e o player
-    const collision = !(pipeRect.right < playerRect.left ||
+    const collisionPipe = !(pipeRect.right < playerRect.left ||
                         pipeRect.left > playerRect.right ||
                         pipeRect.bottom < playerRect.top ||
                         pipeRect.top > playerRect.bottom);
 
-    if (collision) {
+    const collisionPipeVoador = !(pipeVoadorRect.right < playerRect.left ||
+                        pipeVoadorRect.left > playerRect.right ||
+                        pipeVoadorRect.bottom < playerRect.top ||
+                        pipeVoadorRect.top > playerRect.bottom);
+
+    if (collisionPipe || collisionPipeVoador) {
+      controlePontuacao.setPo(false);
+      pontos.style.fontSize = "30px";
       gameOver.style.visibility = 'visible';
       audio.volume = 0;
+
+      reset.style.visibility = 'visible'
 
       pipe.style.animation = "none";
       pipe.style.left = `${pipePosition}px`;
       pipe.src = './img/pipe2.gif';
+
+      pipeVoador.style.animation = "none";
+      pipeVoador.style.left = `${pipeVoadorPosition}px`;
+      pipeVoador.src = './img/pipeVoador2.gif';
 
       player.style.animation = "none";
       player.style.bottom = `${playerPosition}px`;
@@ -79,6 +130,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Função para iniciar o jogo
   const startGame = () => {
+    const { nuvensPosition, arvorePosition, pipePosition, pipeVoadorPosition, playerPosition } = updatePositions();
+
     audio.play();
     audio.volume = 0.1;
 
@@ -86,10 +139,26 @@ document.addEventListener('DOMContentLoaded', () => {
     nuvens.classList.add('animated');
     pipe.classList.add('animated');
     arvore.classList.add('animated');
+    pipeVoador.classList.add('animated');
+
+    controlePontuacao.setPo(true);
 
     jump(); // Executa o salto do jogador
 
     const loop = setInterval(() => {
+
+      // PRECISA SER ARRUMADO, A CADA PONTUAÇÃO AUMENTAR VELOCIDADE POREM CONTINUAR NA POSIÇÃO ATUAL
+
+      // if(pontos.innerHTML === "100"){
+      //   pipe.style.animation = 'pipe-animation 3s linear infinite';
+      //   //pipeVoador.style.animation = 'pipeVoador-animation 8s linear infinite';
+      //   pipe.style.left = pipe.offsetLeft
+      //   console.log(pipePosition);
+      // } else if(pontos.innerHTML === "180"){
+      //   //pipe.style.animation = 'pipe-animation 2s linear infinite';
+      //   //pipeVoador.style.animation = 'pipeVoador-animation 6s linear infinite';
+      // }
+
       if (checkCollision()) {
         clearInterval(loop); // Para o loop se a colisão for detectada
       }
@@ -98,20 +167,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Adiciona o listener para o evento 'Enter' e evita adição repetida de listeners
   const handleKeyDown = (event) => {
+  
     if (event.key === 'Enter') {
-      document.removeEventListener('keydown', handleKeyDown); // Remove o listener após a primeira execução
-      startGame(); // Inicia o jogo
-
-      gameOver.style.visibility = 'hidden';
-      pressEnter.style.visibility = 'hidden';
-      player.src = './img/player.gif';
-      pipe.src = './img/pipe.gif';
-
-      document.addEventListener('keydown', (e) => {
-        if (e.key === 'w' || e.key === 'W') {
-          jump(); // Permite o salto do jogador em resposta à tecla 'W'
-        }
-      });
+        startGame(); // Inicia o jogo
+        gameOver.style.visibility = 'hidden';
+        pressEnter.style.visibility = 'hidden';
+        player.src = './img/player.gif';
+        pipe.src = './img/pipe.gif';
+        pipeVoador.src = './img/pipeVoador.gif';
+        reset.style.visibility = 'hidden'
+    } 
+    if (event.key === 'w' || event.key === 'W') {
+      jump(); // Permite o salto do jogador em resposta à tecla 'W'
+    }
+    
+    if (event.key === 'r' || event.key === 'R') {
+      location.reload();
     }
   };
 
